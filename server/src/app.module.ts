26cfb833267a95585eb1b5e121ffd08a679f1171
ishafiul <shafiulislam20@gmail.com>
2022-05-18 22:00:08 +0600

    some npm pack, db connection auth controller,demo socket getway

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatGateway } from './chat.gateway';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://ishaf:shafiul@cluster0.aecb7.mongodb.net/e2ee?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
